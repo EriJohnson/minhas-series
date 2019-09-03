380a3e0f488f786ba01d94e6cc4e907307cb2d3d
EriJohnson <erijohnsons.fernandes@gmail.com>
2019-09-03 19:31:32 -0300

    Voltar Header p/ tema branco

--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       setOpen(!open)
     }
     return (
-      <Navbar color ='light'  light expand ='md'>
+      <Navbar color ='dark'  light expand ='md'>
         <div className = 'container'>
         <NavbarBrand tag = {Link} to = '/'>Minhas Séries</NavbarBrand>
         <NavbarToggler onClick = {toggle} />
